234e72bd7ab88b6f59d8be22339a4b6f7c469bf0
Nodirbek Isaev <nodirbekisaev5@gmail.com>
2026-09-12 14:56:27 +0900

    feat: python system variables & special methods via dunder

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-a = 147
-print("a: ", a)
-message = "Hello World"
+# Dunder __builtins__, __init__
+message = "PYTHON: Everything is object"
 print(message)
+
+result = type(message)
+print("result:", result)
+
+'''
+  In Python, there are builtin tools:
+  (1) TYPES > int float str list dict
+  (2) FUNCTION > print() len() input() type()
+  (3) CONSTANTS > True False None
+'''
+
+print(dir(__builtins__))
--- train.js
+++ train.js
@@ -1,14 +1,39 @@
+// TASK F:
+
+// Yagona string argumentga ega findDoublers nomli function tuzing
+// Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa
+// true yokida false natija qaytarsin.
+
+// MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
+
+function findDoublers(str) {
+  let letters = [];
+
+  for (let a = 0; a < str.length; a++) {
+    if (letters.includes(str[a])) {
+      return true;
+    }
+
+    letters.push(str[a]);
+  }
+
+  return false;
+}
+
+const result = findDoublers("Hello");
+console.log("Result: ", result);
+
 // TASK E:
 
 // Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
 // MASALAN: getReverse("hello") return qilsin "olleh"
 
-function getReverse(str) {
-  let result = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    result += str[i];
-  }
-  return result;
-}
-const result = getReverse("hello");
-console.log("Result: ", result);
+// function getReverse(str) {
+//   let result = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result += str[i];
+//   }
+//   return result;
+// }
+// const result = getReverse("hello");
+// console.log("Result: ", result);
