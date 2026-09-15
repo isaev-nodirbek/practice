5e411d066e219737a7cde480d4a14ba0927ceafd
Nodirbek Isaev <nodirbekisaev5@gmail.com>
2026-09-15 17:17:29 +0900

    feat: Objects and Classes are done

--- primitive.py
+++ primitive.py
@@ -7,9 +7,9 @@ count_type = type(count)
 print("count:", count, count_type)
 print(f"the count: {count} and type: {count_type}")
 
-# result1 = count.bit_count()  # method
-# result2 = count.numerator  # state
-# print(result1, result2)
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
+print(result1, result2)
 
 print("====== string ========")
 
